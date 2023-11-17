e875b0dae3a240dc39e3e7b8144817439a31bdcd
Efya1235 <Bkumah51@gmail.com>
2023-11-17 02:41:34 -0800

    changes

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,30 +1,24 @@
 #!/usr/bin/python3
-"""A script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument."""
-import sys
+
+"""
+This lists all states from the database hbtn_0e_0_usa
+"""
+
+from sys import argv
 import MySQLdb
 
-
-def main():
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        charset="utf8"
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+            user=argv[1], passwd=argv[2],
+            db=argv[3], host="localhost", port=3306)
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE states.name = '{}' ORDER BY states.id"
+        .format(argv[4])
     )
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
-    cursor = conn.cursor()
-    search = sys.argv[4]
-    query = """SELECT * FROM states WHERE name = '{:s}' ORDER BY id ASC""".format(search)
-    cursor.execute(query)
-    states = cursor.fetchall()
-    for h in states:
-        if h[1] == search:
-            print(h)
-    cursor.close()
-    conn.close()
-
-
-if __name__ == '__main__':
-    main()
+    cur.close()
+    db.close()
